39f00cbcd6e65f3f0c405e99d41995d882c1dcbc
thSteinback <thomas.hsteinback@gmail.com>
2024-06-28 13:37:47 -0300

    Merge branch 'main' of https://github.com/thSteinback/educaFree

--- script.js
+++ script.js
@@ -119,3 +119,105 @@ function displayCourseDetail(container) {
         container.innerHTML = '<p>Curso não encontrado...</p>';
     }
 }
+/* PAGINA DE REGISTRO */
+
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    checkForm();
+}) 
+
+username.addEventListener("blur", () => {
+    checkInputUsername();
+})
+
+email.addEventListener("blur", () => {
+    checkInputEmail();
+})
+
+password.addEventListener("blur", () => {
+    checkInputPassword();
+})
+
+passwordConfirmation.addEventListener("blur", () => {
+    checkInputPasswordConfirmation();
+})
+
+function checkInputUsername(){
+    const usernameValue = username.value;
+
+    if(usernameValue === ""){
+        errorInput(username, "Preencha seu nome.")
+    }else{
+        const formItem = username.parentElement;
+        formItem.className = "form-content"
+    }
+}
+
+function checkInputEmail(){
+    const emailValue = email.value;
+
+    if(emailValue === ""){
+        errorInput(email, "O email é obrigatório.")
+    }else{
+        const formItem = email.parentElement;
+        formItem.className = "form-content"
+    }
+}
+
+function checkInputPassword(){
+    const passwordValue = password.value;
+
+    if(passwordValue === ""){
+        errorInput(password, "A senha é obrigatória.")
+    }else if(passwordValue.lenght < 8){
+        errorInput(password, "A senha precisa ter no mínimo 8 caracteres.")
+    }else{
+        const formItem = password.parentElement;
+        formItem.className = "form-content"
+    }  
+}
+
+function checkInputPasswordConfirmation(){
+    const passwordValue = password.value;
+    const confirmationPasswordValue = passwordConfirmation.value;
+
+    if(confirmationPasswordValue === ""){
+        errorInput(passwordConfirmation, "A confirmação de senha é obrigatória.")
+    }else if(confirmationPasswordValue !== passwordValue){
+        errorInput(passwordConfirmation, "Ase senhas não são iguais.")
+    }else{
+        const formItem = passwordConfirmation.parentElement;
+        formItem.className = "form-content"
+    }
+        
+}
+
+function checkForm(){
+    checkInputUsername();
+    checkInputEmail();
+    checkInputPassword();
+    checkInputPasswordConfirmation();
+
+    const formItems = form.querySelectorAll(".form-content")
+
+    const isValid = [...formItems].every((item) => {
+        return item.className === "form-content"
+    });
+
+}
+
+function errorInput(input, message){
+    const formItem = input.parentElement;
+    const textMessage = formItem.querySelector("a")
+
+    textMessage.innerText = message;
+
+    formItem.clasName = "form-content error"
+}
