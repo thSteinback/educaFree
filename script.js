4b9f710a48d57aee47a10e5b335a33d2d91dca67
MileineFreitas <168874539+MileineFreitas@users.noreply.github.com>
2024-06-28 21:53:57 -0300

    Milene
    Adicionado css da parte de login e cadastro.
    Adicionado js do sanduiche e do carrossel

--- script.js
+++ script.js
@@ -221,3 +221,40 @@ function errorInput(input, message){
 
     formItem.clasName = "form-content error"
 }
+//responsividade tamanho da pagina/mobile//
+function menu() { //troca do icon//
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "imagem/menu.png";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "imagem/fechar.png";
+    }
+  }
+//cards//
+let swiperCards = new Swiper(".card__content", {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+  
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints:{
+      600: {
+        slidesPerView: 2,
+      },
+      968: {
+        slidesPerView: 3,
+      },
+    },
+  });
